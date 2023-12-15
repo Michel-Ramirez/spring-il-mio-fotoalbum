0114c06f22437d28f9c2f2afdc58fbd8787a2216
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-12-15 12:20:32 +0100

    add view detail page

--- src/main/java/org/java/controller/PictureController.java
+++ src/main/java/org/java/controller/PictureController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -23,6 +24,16 @@ public class PictureController {
 
 		model.addAttribute("pictures", pictures);
 		return "index-pictures-list";
+
+	}
+
+	@GetMapping("/picture/{id}")
+	public String detailPicture(Model model, @PathVariable int id) {
+
+		Picture picture = pictureService.findById(id);
+
+		model.addAttribute("picture", picture);
+		return "detail-picture";
 
 	}
 
